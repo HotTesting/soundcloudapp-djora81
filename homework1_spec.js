1befd1f6bf8577a03970f577dcdbae7c0e8e1208
djora81 <djora81@mail.ru>
2016-10-11 14:50:41 +0300

    Homework1
    changes in third part

--- homework1_spec.js
+++ homework1_spec.js
@@ -5,7 +5,7 @@ describe('First test', function () {
     it('should click play button', function () {
         browser.get('https://soundcloud-ngrx.herokuapp.com/');
 
-        let playClick = $$('.btn.btn--icon.btn--play').get(2).click();
+        $$('.btn.btn--icon.btn--play').get(2).click();
         browser.sleep(2000);
         
         let openPlayer = $('.player-controls__title');
@@ -16,8 +16,8 @@ describe('First test', function () {
     it('detailed list second artist', function () {
         browser.get('https://soundcloud-ngrx.herokuapp.com/');
 
-        let openList = $$('.track-card__username').get(1).click();
-        let trackPlay = $$('.btn.btn--icon.btn--play').get(0).click();
+        $$('.track-card__username').get(1).click();
+        $$('.btn.btn--icon.btn--play').get(0).click();
         let openPlayer = $('.player-controls__title');
         expect(openPlayer.isDisplayed()).toBe(true);
         browser.sleep(2000);
@@ -26,14 +26,15 @@ describe('First test', function () {
     it('should find the track text', function () {
         browser.get('https://soundcloud-ngrx.herokuapp.com/');
 
-        let openSearch = $$('.btn.btn--icon.btn--search-alt').click();
-        let findTrack = $$('.search-form__input.ng-pristine.ng-invalid.ng-touched').sendKeys('paul van dyk');
-        findTrack.submit();
-
-        let firstText = $$('.track-card__title').getText();
-        expect(firstText).toBe('paul van dyk');
-        let title = $$('.content-header__title').getText();
-        expect(title).toBe('paul van dyk');
+        $('.btn.btn--icon.btn--search-alt').click();
+        //browser.sleep(2000);
+        let search = ('Paul van Dyk');
+        $('.search-form__input').sendKeys(search).submit();
+        
+        let firstText = $$('.track-card__title').first().getText();
+        expect(firstText).toMatch(search);
+        let title = $('.content-header__title').getText();
+        expect(title).toMatch(search);
         
     })
 });
