1c2e5147cf70e7b6fa351ff13efa1f7ee235a8e8
djora81 <djora81@mail.ru>
2016-10-11 12:15:02 +0300

    Homework1
    some problem in 3 part

--- homework1_spec.js
+++ homework1_spec.js
@@ -2,22 +2,38 @@
 
 describe('First test', function () {
 
-    it('should click play buttom', function () {
+    it('should click play button', function () {
         browser.get('https://soundcloud-ngrx.herokuapp.com/');
 
-        let playClick = $$('.btn.btn--icon.btn--play').get(2);
-        playClick.click();
+        let playClick = $$('.btn.btn--icon.btn--play').get(2).click();
         browser.sleep(2000);
-
-        let thirdTrack = $$('.track-card__title').get(2);
-        let trackName = thirdTrack.getText();
-        expect(trackName.getText()).toBe('Few Nolder – THUMP Guest Mix');
-
-        let openPlayer = $('.player-controls');
-        let title = $('.player-controls__title')
+        
+        let openPlayer = $('.player-controls__title');
         expect(openPlayer.isDisplayed()).toBe(true);
-        expect(title.getText()).toBe('Few Nolder – THUMP Guest Mix');
-      
+
+    });
+    
+    it('detailed list second artist', function () {
+        browser.get('https://soundcloud-ngrx.herokuapp.com/');
+
+        let openList = $$('.track-card__username').get(1).click();
+        let trackPlay = $$('.btn.btn--icon.btn--play').get(0).click();
+        let openPlayer = $('.player-controls__title');
+        expect(openPlayer.isDisplayed()).toBe(true);
+        browser.sleep(2000);
     });
 
+    it('should find the track text', function () {
+        browser.get('https://soundcloud-ngrx.herokuapp.com/');
+
+        let openSearch = $$('.btn.btn--icon.btn--search-alt').click();
+        let findTrack = $$('.search-form__input.ng-pristine.ng-invalid.ng-touched').sendKeys('paul van dyk');
+        findTrack.submit();
+
+        let firstText = $$('.track-card__title').getText();
+        expect(firstText).toBe('paul van dyk');
+        let title = $$('.content-header__title').getText();
+        expect(title).toBe('paul van dyk');
+        
+    })
 });
